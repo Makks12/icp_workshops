9122c12b5f745a3a49cd20a5f3a9105ac2ed677b
Maksymilian Łojszczyk <maksss.lojszczyk@gmail.com>
2025-03-13 17:48:37 +0000

    Frontend edit

--- src/icp_workshops_frontend/src/App.vue
+++ src/icp_workshops_frontend/src/App.vue
@@ -1,14 +1,20 @@
 <script setup>
 import { ref } from 'vue';
 import { icp_workshops_backend } from 'declarations/icp_workshops_backend/index';
-let greeting = ref('');
+let translation = ref('');
 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
-  const name = target.querySelector('#name').value;
-  await icp_workshops_backend.greet(name).then((response) => {
-    greeting.value = response;
+  const text = target.querySelector('#text').value;
+  await icp_workshops_backend.translate(text).then((response) => {
+    if ("Err" in response){
+      alert("BŁĄD: " + response.Err)
+     
+      return
+    }
+   
+    translation.value = response.Ok;
   });
 }
 </script>
@@ -19,10 +25,10 @@ async function handleSubmit(e) {
     <br />
     <br />
     <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
+      <label for="text">Translate to german: &nbsp;</label>
+      <input id="text" alt="text" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="greeting">{{ greeting }}</section>
+    <section id="greeting">{{ translation }}</section>
   </main>
 </template>
